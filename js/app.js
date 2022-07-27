e431a5b271a1712db1b0533052f87917f3f40177
Amy Pierce <diablokitty@gmail.com>
2022-07-27 14:08:44 -0700

    function for first question

--- js/app.js
+++ js/app.js
@@ -6,11 +6,7 @@
 //by prompting the user to guess a number.
 //Indicates through an alert if the guess is “too high” or “too low”.
 //It should give the user exactly four opportunities to get the correct answer.
-//After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.
-//a 7th question Add a 7th question that has multiple possible correct answers that are stored in an array.
-//Give the user 6 attempts to guess the correct answer.
-//The guesses will end once the user guesses a correct answer or they run out of attempts.
-//Display all the possible correct answers to the user.
+
 //Consider using a loop of some sort for this question.
 //adjust the final score to include the two new questions.
 
@@ -21,55 +17,60 @@ alert('Welcome to all about me!');
 let username = prompt('What is your name?');
 console.log(`Question: What is your name? Response: ${username}`);
 let score = 0;
-let ques1 = prompt(`Hi ${username}, I'm going to ask you a series of yes or no questions. For each correct answer, you get one point. Are you ready?`).toLowerCase();
-//console.log(`Question: Are you ready? Response: ${ques1}`);
 
-if(ques1 === 'yes' || ques1 === 'y' ) {
-  score = score + 1;
-  alert(`Ok ${username} that was an easy one. Your score is ${score}.`);
-  //console.log(`Ok ${username} that was an easy one. Your score is ${score}.`);
+function firstQuestion(username) {
+  let ques1 = prompt(`Hi ${username}, I'm going to ask you a series of yes or no questions. For each correct answer, you get one point. Are you ready?`).toLowerCase();
+  //console.log(`Question: Are you ready? Response: ${ques1}`);
+
+  if (ques1 === 'yes' || ques1 === 'y') {
+    score = score + 1;
+    alert(`Ok ${username} that was an easy one. Your score is ${score}.`);
+    //console.log(`Ok ${username} that was an easy one. Your score is ${score}.`);
+  }
+
+  return score;
 }
-alert(`Next question!`);
+alert('Next question!');
 
 let ques2 = prompt('Will I ever be a super awesome code fiend?').toLowerCase();
 //console.log(`Question: Will I ever be a super awesome code fiend? Response: ${ques2});
 
-if(ques2 === 'yes' || ques2 === 'y' ) {
+if (ques2 === 'yes' || ques2 === 'y') {
   score = score + 1;
   alert(`Oh ${username}, your confidence is inspiring! Your score is ${score}.`);
   //console.log(`Oh ${username}, your confidence is inspiring! Your score is ${score}.`);
 }
 else {
-  alert(`Hmm. Well I don't know about that. Let's try another one.`);
+  alert('Hmm. Well I don\'t know about that. Let\'s try another one.');
 }
 let ques3 = prompt(`So ${username}, do you think I'll ever meet a plane I don't want to get on?`).toLowerCase();
 //console.log(`Question: Do you think I'll ever meet a plane I don't want to get on? Response: ${ques3});
 
-if(ques3 === 'yes' || ques3 === 'y' ) {
+if (ques3 === 'yes' || ques3 === 'y') {
   score = score + 1;
   alert(`Well, you're right, ${username}. There are a few places I wouldn't want to end up! Your score is ${score}.`);
   //console.log(`Well, you're right, ${username}. There are a few places I wouldn't want to end up! Your score is ${score}.`);
 }
 else {
-  alert(`Oh I don't know, there are a few places I wouldn't want to end up!`);
+  alert('Oh I don\'t know, there are a few places I wouldn\'t want to end up!');
 }
 
 let ques4 = prompt(`Next question ${username}, is purple my favorite color?`).toLowerCase();
 //console.log(`Question: Is purple my favorite color? Response: ${ques4}`);
 
-if(ques4 === 'yes' || ques4 === 'y' ) {
+if (ques4 === 'yes' || ques4 === 'y') {
   score = score + 1;
   alert(`What gave it away?!? Your score is ${score}.`);
   //console.log(`What gave it away?!? Your score is ${score}.`);
 }
 else {
-  alert(`It certainly is, but blue is my second favorite, so I understand the confusion.`);
+  alert(`It certainly is, ${username}, but blue is my second favorite, so I understand the confusion.`);
 }
 
 let ques5 = prompt(`Listen ${username}, I need a serious answer. Do you like my new hat?`).toLowerCase();
 //console.log(`Question: Do you like my new hat? Response: ${ques5}`);
 
-if(ques5 === 'yes' || ques5 === 'y' ) {
+if (ques5 === 'yes' || ques5 === 'y') {
   score = score + 1;
   alert(`It IS kind of a great party hat. Your score is ${score}.`);
   //console.log(`It IS kind of a great party hat. Your score is ${score}.`);
@@ -86,24 +87,26 @@ let ques6 = prompt(`Let's try something else, ${username}. I'm thinking of a num
 //ques6 = Number(ques6); if we wanted to use strictly not equal this code would go here and at lines 93 and 96
 let guesses = 4;
 
-while(ques6 != goodguess && guesses >= 1) {
+while (ques6 !== goodguess && guesses >= 1) {
   //console.log(`for loop`);
-  if(ques6 > goodguess){
+  if (ques6 > goodguess) {
     ques6 = prompt(`Too high, ${username}. You have ${guesses} tries left. Next guess?`);
     guesses--;
   }
-  else if (ques6 < goodguess){
+  else if (ques6 < goodguess) {
     ques6 = prompt(`Too low, ${username}. You have ${guesses} tries left. Try again?`);
     guesses--;
   }
 }
 //console.log('You\'re out of the loop.');
-if(ques6==goodguess){
+if (ques6 === goodguess) {
   score++;
   alert(`You got it, ${username}! Your score is ${score}.`);
 }
-if(guesses===0) {
+if (guesses === 0) {
   alert(`Oh no, ${username}! You ran out of guesses! The answer was ${goodguess}.`);
+
+  firstQuestion(username);
 }
 
 alert(`${username}, you've been a great sport! Your ending score is ${score}, but don't worry, there's no prize except the satisfaction of a game well played.`);
